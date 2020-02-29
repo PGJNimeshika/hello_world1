15e1d7a51173deb29e23c5c4f3c0c2da218e3dfb
Jehani <j.nimeshika.sandu@gmail.com>
2020-02-28 23:14:09 -0800

    first commit

---
+++ first1/src/first1/main.java
@@ -0,0 +1,11 @@
+package first1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my first project at sliit");
+
+	}
+
+}
